4c3070d9bf1014040690d10b25c78e0d6df04a88
cevherbakan <cevherbakan@gmail.com>
2021-03-05 22:04:48 +0300

    created

---
+++ ProductExampleEntity/Migrations/20210304131203_Product.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductExampleEntity.Migrations
+{
+    public partial class Product : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(250)", nullable: true),
+                    Explanation = table.Column<string>(type: "nvarchar(250)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
